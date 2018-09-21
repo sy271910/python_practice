fc79683b59f4991d95300f60c264280b8be06008
史远 <shiyuan@films.cn>
2018-09-21 15:18:01 +0800

    'p1'

--- practice.py
+++ practice.py
@@ -121,12 +121,3 @@ list=[1,2,3,4]
 it = iter(list)    # 创建迭代器对象
 for x in it:
     print(x)
-
-import sys  #python标准库模块
-list = [1, 2, 3, 4]
-it = iter(list)  # 创建迭代器对象
-while True:
-    try:
-        print(next(it))
-    except StopIteration:
-        sys.exit()
