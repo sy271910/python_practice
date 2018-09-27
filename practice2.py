fbe16ef0c872ea9becbdfa15f9be5d6d59ff986d
史远 <shiyuan@films.cn>
2018-09-27 17:58:31 +0800

    9.27

--- practice2.py
+++ practice2.py
@@ -1,4 +1,8 @@
 # -*- coding: UTF-8 -
+from enum import Enum,unique
+
+
+
 
 # 定义一个函数
 #必选参数在前 默认参数在后 调用时默认参数可省略，默认参数必须指向不变对象
@@ -28,9 +32,11 @@ sum = lambda arg1, arg2: arg1 + arg2
 print("相加后的值为 : ", sum(10, 20))
 printinfo(70, 60, 50)
 
-x = set('runoob')
+# 创建一个集合
+aa={'a','a','b'}
+print(aa)
+x = set('aabbcc')
 print(x)
-
 
 
 # python 面向对象
@@ -54,7 +60,6 @@ class Student(object):
         self.name = name
         self.score = score
 
-
     def get_grade(self):
         if self.score >= 90:
             return 'A'
@@ -62,7 +67,6 @@ class Student(object):
             return 'B'
         else:
             return 'C'
-
 
 lisa = Student('Lisa', 99)
 bart = Student('Bart', 59)
@@ -89,17 +93,51 @@ def run_twice(animal):
 
 run_twice(Animal())
 
-# map()函数接收两个参数，一个是函数，一个是Iterable，map将传入的函数依次作用到序列的每个元素，并把结果作为新的Iterator返回
+# map()
 def f(x):
     return x*x
 
 print(list(map(f, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
 print(list(map(str, [1, 2, 3, 4, 5, 6, 7, 8, 9])))
-
+#filter()
 def is_odd(n):
     return n % 2 == 1
 
 print(list(filter(is_odd, [1, 2, 4, 5, 6, 9, 10, 15])))
+
+# __slots__
+class uzi():
+    __slots__ = ('name', 'age')
+
+uu=uzi()
+uu.name='简自豪'
+#@property
+class Student(object):
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
+
+s=Student()
+s.score=99
+#枚举
+class Color(Enum):
+    red = 1
+    yellow = 2
+print(Color(1))
+for color in Color.__members__.items():
+     print(color)
+
+
+
+
 
 def python_sy():
     print("史远")
